1fdb9b7c15426bcbc6cd027b89fe7d9583d8fe3b
Francis Bulu <Francis.Bulu@student.intecbrussel.be>
2020-10-29 14:56:13 +0100

    Add project files.

---
+++ OverloadingSolo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingSolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Print(11);
+            Print(0, "val");
+        }
+        static void Print(int a)
+        {
+            Console.WriteLine("Value: " + a);
+        }
+        static void Print(int b, string value)
+        {
+            Console.WriteLine("New value: "+ value + b);
+        }
+    }
+}
